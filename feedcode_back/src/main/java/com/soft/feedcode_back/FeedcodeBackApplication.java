486c3b8c54a8f20e73235ca1b2fa0d4a1a2fe9e1
Madhubhashana Mahindarathne <104136158+Madhubhashana99@users.noreply.github.com>
2024-07-10 19:07:02 +0530

    begin

---
+++ feedcode_back/src/main/java/com/soft/feedcode_back/FeedcodeBackApplication.java
@@ -0,0 +1,13 @@
+package com.soft.feedcode_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedcodeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedcodeBackApplication.class, args);
+	}
+
+}
---
+++ feedcode_back/src/test/java/com/soft/feedcode_back/FeedcodeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soft.feedcode_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedcodeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
